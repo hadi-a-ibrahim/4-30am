0be8ba24f6b29d655890f1de194040099daf8fc8
Hadi <hadi21001@gmail.com>
2025-08-26 17:22:32 +0530

    fix: rewrite Scripts.tsx without === true

--- src/layouts/RootLayout/Scripts.tsx
+++ src/layouts/RootLayout/Scripts.tsx
@@ -1,4 +1,26 @@
+import React from "react"
+import Script from "next/script"
+import { CONFIG } from "../../../site.config"
+
 export default function Scripts() {
-  // Temporarily disabled to unblock builds. We'll re-enable later.
-  return null;
+  const gaOn = !!CONFIG?.googleAnalytics?.enable
+  const id = CONFIG?.googleAnalytics?.config?.measurementId || ""
+  if (!gaOn || !id) return null
+
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${id}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${id}');
+        `}
+      </Script>
+    </>
+  )
 }
