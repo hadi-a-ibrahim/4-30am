a38cb85d4c11e24dc5bc064282fdeaa4b3072d7d
Hadi <hadi21001@gmail.com>
2025-08-26 17:14:40 +0530

    chore: temp disable Scripts to unblock Vercel build

--- src/layouts/RootLayout/Scripts.tsx
+++ src/layouts/RootLayout/Scripts.tsx
@@ -1,31 +1,4 @@
-import React from "react"
-import Script from "next/script"
-import { CONFIG } from "../../../site.config"
-
-const Scripts: React.FC = () => {
-  const gaOn = !!CONFIG?.googleAnalytics?.enable
-  const id = CONFIG?.googleAnalytics?.config?.measurementId || ""
-
-  return (
-    <>
-      {gaOn && id && (
-        <>
-          <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=${id}`}
-            strategy="afterInteractive"
-          />
-          <Script id="ga-init" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${id}');
-            `}
-          </Script>
-        </>
-      )}
-    </>
-  )
+export default function Scripts() {
+  // Temporarily disabled to unblock builds. We'll re-enable later.
+  return null;
 }
-
-export default Scripts
