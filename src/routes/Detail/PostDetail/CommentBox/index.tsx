145ecd8447b74c11d991e6415ed6505f0d1f1d02
Hadi <hadi21001@gmail.com>
2025-08-28 09:36:40 +0530

    fixed commentbox error

--- src/routes/Detail/PostDetail/CommentBox/index.tsx
+++ src/routes/Detail/PostDetail/CommentBox/index.tsx
@@ -2,19 +2,22 @@
 import dynamic from "next/dynamic"
 import { CONFIG } from "site.config"
 
-// Dynamically import Utterances; it has **no props** now
+// Accept an optional `data` prop to satisfy existing call sites,
+// but we don't actually use it (Utterances reads from CONFIG).
+type Props = { data?: unknown }
+
+// Dynamically import Utterances; it has no required props
 const UtterancesComponent = dynamic(
   () => import("./Utterances").then((mod) => mod.default),
   { ssr: false }
 )
 
-export default function CommentBox() {
+export default function CommentBox(_props: Props) {
   // if all comment systems are disabled, render nothing
   if (!CONFIG?.utterances?.enable && !CONFIG?.cusdis?.enable) {
     return null
   }
 
-  // your setup uses Utterances
   if (CONFIG?.utterances?.enable) {
     return <UtterancesComponent />
   }
